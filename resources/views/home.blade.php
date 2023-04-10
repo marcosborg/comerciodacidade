4190a31f1cdab086b1a4a426284d43ad6fef0ee2
marcosborg <m.borges.mail@gmail.com>
2023-04-10 18:44:08 +0100

    Upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,42 +10,47 @@
 
                 <div class="card-body">
                     <p><b>Logado como: </b>{{ auth()->user()->email }}</p>
-                    <p><b>Plano atual: </b>{{ $user->subscription->subscription_type->plan->name }}</p>
-                    <p><b>Última renovação: </b>{{ $user->subscription->start_date }}</p>
-                    <p><b>Proxima renovação: </b>{{ $user->subscription->end_date }}</p>
+                    <p><b>Plano atual: </b>{{ $user->subscription ? $user->subscription->subscription_type->plan->name : 'Ainda não fez nenhuma subscrição' }}</p>
+                    <p><b>Última renovação: </b>{{ $user->subscription ? $user->subscription->start_date : 'Ainda não fez nenhuma subscrição' }}</p>
+                    <p><b>Proxima renovação: </b>{{ $user->subscription ? $user->subscription->end_date : 'Ainda não fez nenhuma subscrição' }}</p>
                     <p>
-                        <button class="btn btn-primary" id="btn-renew" type="button" data-toggle="collapse"
-                            data-target="#collapsePayment" aria-expanded="false">
+                        <button class="btn btn-primary" id="btn-renew" type="button" data-toggle="collapse" data-target="#collapsePayment" aria-expanded="false">
                             Renovar
                         </button>
                     </p>
                     <div class="collapse" id="collapsePayment">
                         <ul class="nav nav-tabs" role="tablist">
-                            @foreach ($plans as $plan)
+                            @foreach ($plans as $key => $plan)
                             <li class="nav-item">
-                                <a class="nav-link {{ $user->subscription->subscription_type->plan_id == $plan->id ? 'active' : '' }}"
-                                    data-toggle="tab" href="#tab-{{ $plan->id }}" role="tab">{{
+                                <a class="nav-link 
+                                @if ($user->subscription)
+                                {{ $user->subscription->subscription_type->plan_id == $plan->id ? 'active' : '' }}
+                                @else
+                                {{ $key == 0 ? 'active' : '' }}
+                                @endif
+                                " data-toggle="tab" href="#tab-{{ $plan->id }}" role="tab">{{
                                     $plan->name }}</a>
                             </li>
                             @endforeach
                         </ul>
                         <div class="tab-content">
-                            @foreach ($plans as $plan)
-                            <div class="tab-pane fade {{ $user->subscription->subscription_type->plan_id == $plan->id ? 'show active' : '' }}"
-                                id="tab-{{ $plan->id }}" role="tabpanel">
+                            @foreach ($plans as $key => $plan)
+                            <div class="tab-pane fade 
+                            @if ($user->subscription)
+                            {{ $user->subscription->subscription_type->plan_id == $plan->id ? 'show active' : '' }}
+                            @else
+                            {{ $key == 0 ? 'show active' : '' }}
+                            @endif
+                            " id="tab-{{ $plan->id }}" role="tabpanel">
                                 <div class="card">
                                     <div class="card-body">
                                         <div class="row">
                                             <div class="col-md-6">
                                                 @foreach ($plan->subscriptionTypes as $key => $subscriptionType)
                                                 <div class="form-check">
-                                                    <input class="form-check-input" type="radio"
-                                                        name="subscription_type"
-                                                        id="subscription_type_{{ $subscriptionType->id }}"
-                                                        value="{{ $subscriptionType->id }}" {{ $key==0 ? 'checked' : ''
+                                                    <input class="form-check-input" type="radio" name="subscription_type" id="subscription_type_{{ $subscriptionType->id }}" value="{{ $subscriptionType->id }}" {{ $key==0 ? 'checked' : ''
                                                         }}>
-                                                    <label class="form-check-label"
-                                                        for="subscription_type_{{ $subscriptionType->id }}">
+                                                    <label class="form-check-label" for="subscription_type_{{ $subscriptionType->id }}">
                                                         {{ $subscriptionType->months }} meses com {{
                                                         $subscriptionType->discount
                                                         }}% de
@@ -75,6 +80,7 @@
                 </div>
 
                 <div class="card-body">
+                    @if ($user->subscription && $user->subscription->subscriptionPayments)
                     <table class="table">
                         <thead>
                             <tr>
@@ -95,6 +101,11 @@
                             @endforeach
                         </tbody>
                     </table>
+                    @else
+                    <div class="alert alert-primary" role="alert">
+                        Ainda não existem pagamentos.
+                    </div>
+                    @endif
                 </div>
             </div>
         </div>
@@ -116,7 +127,7 @@
 @parent
 <script>
     $(() => {
-        $('#collapsePayment').on('shown.bs.collapse', function () {
+        $('#collapsePayment').on('shown.bs.collapse', function() {
             $('#btn-renew').hide();
         })
 
@@ -133,7 +144,7 @@
             let subscriptionType = resp;
             let totalWithoutDiscount = subscriptionType.months * subscriptionType.plan.price;
             let discount = subscriptionType.discount;
-            let totalWithDiscount = (totalWithoutDiscount * discount)/100;
+            let totalWithDiscount = (totalWithoutDiscount * discount) / 100;
             let total = totalWithoutDiscount - totalWithDiscount;
             console.log({
                 totalWithoutDiscount: totalWithoutDiscount,
@@ -154,12 +165,14 @@
                 "Cookie": "ARRAffinity=cc3c6651acdd3282d60a76036929f0b096836d3038b01b137cfbdaf09c0a1429; ASP.NET_SessionId=elrpt2qsz5hbmcs1ipvs4n0k"
             },
         };
-        $.ajax(settings).done(function (response) {
+        $.ajax(settings).done(function(response) {
             console.log(response);
             window.open(response, '_blank', 'width=500,height=500');
         });
     }
 
-    console.log({!! $plans !!})
+    console.log({
+        !!$plans!!
+    })
 </script>
 @endsection
