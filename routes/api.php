86013536ab5596d74f494c88581bb04112b97f9d
marcosborg <m.borges.mail@gmail.com>
2023-04-19 08:28:04 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+Route::post('login', 'Api\\AuthController@login');
+Route::post('register', 'Api\\AuthController@register');
+
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
     // Subscription Payment
     Route::apiResource('subscription-payments', 'SubscriptionPaymentApiController');
+
+    // Shop Category
+    Route::post('shop-categories/media', 'ShopCategoryApiController@storeMedia')->name('shop-categories.storeMedia');
+    Route::apiResource('shop-categories', 'ShopCategoryApiController');
 });
 
 Route::middleware('verifyAntiPhishingKey')->prefix('callback')->group(function(){
