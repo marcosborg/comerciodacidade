74bece40d8e8491687db9aa31760af6df9d07e52
marcosborg <m.borges.mail@gmail.com>
2023-05-09 09:25:11 +0100

    Update

--- resources/views/admin/myProducts/create.blade.php
+++ resources/views/admin/myProducts/create.blade.php
@@ -1,120 +1,162 @@
-<script>console.log({!! $shop_product_sub_categories !!})</script>
+<script>
+  console.log({!! $shop_product_sub_categories !!})
+</script>
 
 @extends('layouts.admin')
 @section('content')
 
 <div class="card">
-    <div class="card-header">
-        {{ trans('global.create') }} {{ trans('cruds.myProduct.title_singular') }}
-    </div>
+  <div class="card-header">
+    {{ trans('global.create') }} {{ trans('cruds.myProduct.title_singular') }}
+  </div>
 
-    <div class="card-body">
-        <form method="POST" action="{{ route("admin.shop-products.store") }}" enctype="multipart/form-data">
-            @csrf
-            <input type="hidden" name="myProduct" value="1">
-            <div class="form-group">
-                <label class="required" for="name">{{ trans('cruds.shopProduct.fields.name') }}</label>
-                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
-                @if($errors->has('name'))
-                    <span class="text-danger">{{ $errors->first('name') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.shopProduct.fields.name_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label for="reference">{{ trans('cruds.shopProduct.fields.reference') }}</label>
-                <input class="form-control {{ $errors->has('reference') ? 'is-invalid' : '' }}" type="text" name="reference" id="reference" value="{{ old('reference', '') }}">
-                @if($errors->has('reference'))
-                    <span class="text-danger">{{ $errors->first('reference') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.shopProduct.fields.reference_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label for="description">{{ trans('cruds.shopProduct.fields.description') }}</label>
-                <textarea class="form-control ckeditor {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description" id="description">{!! old('description') !!}</textarea>
-                @if($errors->has('description'))
-                    <span class="text-danger">{{ $errors->first('description') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.shopProduct.fields.description_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label for="photos">{{ trans('cruds.shopProduct.fields.photos') }}</label>
-                <div class="needsclick dropzone {{ $errors->has('photos') ? 'is-invalid' : '' }}" id="photos-dropzone">
-                </div>
-                @if($errors->has('photos'))
-                    <span class="text-danger">{{ $errors->first('photos') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.shopProduct.fields.photos_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label for="shop_product_categories">{{ trans('cruds.shopProduct.fields.shop_product_categories') }}</label>
-                <div style="padding-bottom: 4px">
-                    <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
-                    <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
-                </div>
-                <select class="form-control select2 {{ $errors->has('shop_product_categories') ? 'is-invalid' : '' }}" name="shop_product_categories[]" id="shop_product_categories" multiple>
-                    @foreach($shop_product_categories as $id => $shop_product_category)
-                        <option value="{{ $id }}" {{ in_array($id, old('shop_product_categories', [])) ? 'selected' : '' }}>{{ $shop_product_category }}</option>
-                    @endforeach
-                </select>
-                @if($errors->has('shop_product_categories'))
-                    <span class="text-danger">{{ $errors->first('shop_product_categories') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.shopProduct.fields.shop_product_categories_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label for="shop_product_sub_categories">{{ trans('cruds.shopProduct.fields.shop_product_sub_categories') }}</label>
-                <div style="padding-bottom: 4px">
-                    <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
-                    <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
-                </div>
-                <select class="form-control select2 {{ $errors->has('shop_product_sub_categories') ? 'is-invalid' : '' }}" name="shop_product_sub_categories[]" id="shop_product_sub_categories" multiple>
-                    @foreach($shop_product_sub_categories as $id => $shop_product_sub_category)
-                        <option value="{{ $id }}" {{ in_array($id, old('shop_product_sub_categories', [])) ? 'selected' : '' }}>{{ $shop_product_sub_category }}</option>
-                    @endforeach
-                </select>
-                @if($errors->has('shop_product_sub_categories'))
-                    <span class="text-danger">{{ $errors->first('shop_product_sub_categories') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.shopProduct.fields.shop_product_sub_categories_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label for="price">{{ trans('cruds.shopProduct.fields.price') }}</label>
-                <input class="form-control {{ $errors->has('price') ? 'is-invalid' : '' }}" type="number" name="price" id="price" value="{{ old('price', '') }}" step="0.01">
-                @if($errors->has('price'))
-                    <span class="text-danger">{{ $errors->first('price') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.shopProduct.fields.price_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label class="required" for="tax_id">{{ trans('cruds.shopProduct.fields.tax') }}</label>
-                <select class="form-control select2 {{ $errors->has('tax') ? 'is-invalid' : '' }}" name="tax_id" id="tax_id" required>
-                    @foreach($taxes as $id => $entry)
-                        <option value="{{ $id }}" {{ old('tax_id') == $id ? 'selected' : '' }}>{{ $entry }}</option>
-                    @endforeach
-                </select>
-                @if($errors->has('tax'))
-                    <span class="text-danger">{{ $errors->first('tax') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.shopProduct.fields.tax_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <div class="form-check {{ $errors->has('state') ? 'is-invalid' : '' }}">
-                    <input type="hidden" name="state" value="0">
-                    <input class="form-check-input" type="checkbox" name="state" id="state" value="1" {{ old('state', 0) == 1 || old('state') === null ? 'checked' : '' }}>
-                    <label class="form-check-label" for="state">{{ trans('cruds.shopProduct.fields.state') }}</label>
-                </div>
-                @if($errors->has('state'))
-                    <span class="text-danger">{{ $errors->first('state') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.shopProduct.fields.state_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <button class="btn btn-danger" type="submit">
-                    {{ trans('global.save') }}
-                </button>
-            </div>
-        </form>
-    </div>
+  <div class="card-body">
+    <form method="POST" action="{{ route("admin.shop-products.store") }}" enctype="multipart/form-data">
+      @csrf
+      <input type="hidden" name="myProduct" value="1">
+      <div class="form-group">
+        <label class="required" for="name">{{ trans('cruds.shopProduct.fields.name') }}</label>
+        <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name"
+          value="{{ old('name', '') }}" required>
+        @if($errors->has('name'))
+        <span class="text-danger">{{ $errors->first('name') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.name_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label for="reference">{{ trans('cruds.shopProduct.fields.reference') }}</label>
+        <input class="form-control {{ $errors->has('reference') ? 'is-invalid' : '' }}" type="text" name="reference"
+          id="reference" value="{{ old('reference', '') }}">
+        @if($errors->has('reference'))
+        <span class="text-danger">{{ $errors->first('reference') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.reference_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label for="description">{{ trans('cruds.shopProduct.fields.description') }}</label>
+        <textarea class="form-control ckeditor {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description"
+          id="description">{!! old('description') !!}</textarea>
+        @if($errors->has('description'))
+        <span class="text-danger">{{ $errors->first('description') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.description_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label for="photos">{{ trans('cruds.shopProduct.fields.photos') }}</label>
+        <div class="needsclick dropzone {{ $errors->has('photos') ? 'is-invalid' : '' }}" id="photos-dropzone">
+        </div>
+        @if($errors->has('photos'))
+        <span class="text-danger">{{ $errors->first('photos') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.photos_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label for="shop_product_categories">{{ trans('cruds.shopProduct.fields.shop_product_categories') }}</label>
+        <div style="padding-bottom: 4px">
+          <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
+          <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all')
+            }}</span>
+        </div>
+        <select class="form-control select2 {{ $errors->has('shop_product_categories') ? 'is-invalid' : '' }}"
+          name="shop_product_categories[]" id="shop_product_categories" multiple>
+          @foreach($shop_product_categories as $id => $shop_product_category)
+          <option value="{{ $id }}" {{ in_array($id, old('shop_product_categories', [])) ? 'selected' : '' }}>{{
+            $shop_product_category }}</option>
+          @endforeach
+        </select>
+        @if($errors->has('shop_product_categories'))
+        <span class="text-danger">{{ $errors->first('shop_product_categories') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.shop_product_categories_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label for="shop_product_sub_categories">{{ trans('cruds.shopProduct.fields.shop_product_sub_categories')
+          }}</label>
+        <div style="padding-bottom: 4px">
+          <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
+          <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all')
+            }}</span>
+        </div>
+        <select class="form-control select2 {{ $errors->has('shop_product_sub_categories') ? 'is-invalid' : '' }}"
+          name="shop_product_sub_categories[]" id="shop_product_sub_categories" multiple>
+          @foreach($shop_product_sub_categories as $id => $shop_product_sub_category)
+          <option value="{{ $id }}" {{ in_array($id, old('shop_product_sub_categories', [])) ? 'selected' : '' }}>{{
+            $shop_product_sub_category }}</option>
+          @endforeach
+        </select>
+        @if($errors->has('shop_product_sub_categories'))
+        <span class="text-danger">{{ $errors->first('shop_product_sub_categories') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.shop_product_sub_categories_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label for="price">{{ trans('cruds.shopProduct.fields.price') }}</label>
+        <input class="form-control {{ $errors->has('price') ? 'is-invalid' : '' }}" type="number" name="price"
+          id="price" value="{{ old('price', '') }}" step="0.01">
+        @if($errors->has('price'))
+        <span class="text-danger">{{ $errors->first('price') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.price_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label class="required" for="tax_id">{{ trans('cruds.shopProduct.fields.tax') }}</label>
+        <select class="form-control select2 {{ $errors->has('tax') ? 'is-invalid' : '' }}" name="tax_id" id="tax_id"
+          required>
+          @foreach($taxes as $id => $entry)
+          <option value="{{ $id }}" {{ old('tax_id')==$id ? 'selected' : '' }}>{{ $entry }}</option>
+          @endforeach
+        </select>
+        @if($errors->has('tax'))
+        <span class="text-danger">{{ $errors->first('tax') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.tax_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label for="youtube">{{ trans('cruds.shopProduct.fields.youtube') }}</label>
+        <input class="form-control {{ $errors->has('youtube') ? 'is-invalid' : '' }}" type="text" name="youtube"
+          id="youtube" value="{{ old('youtube', '') }}">
+        @if($errors->has('youtube'))
+        <span class="text-danger">{{ $errors->first('youtube') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.youtube_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label for="attachment_name">{{ trans('cruds.shopProduct.fields.attachment_name') }}</label>
+        <input class="form-control {{ $errors->has('attachment_name') ? 'is-invalid' : '' }}" type="text"
+          name="attachment_name" id="attachment_name" value="{{ old('attachment_name', '') }}">
+        @if($errors->has('attachment_name'))
+        <span class="text-danger">{{ $errors->first('attachment_name') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.attachment_name_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <label for="attachment">{{ trans('cruds.shopProduct.fields.attachment') }}</label>
+        <div class="needsclick dropzone {{ $errors->has('attachment') ? 'is-invalid' : '' }}" id="attachment-dropzone">
+        </div>
+        @if($errors->has('attachment'))
+        <span class="text-danger">{{ $errors->first('attachment') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.attachment_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <div class="form-check {{ $errors->has('state') ? 'is-invalid' : '' }}">
+          <input type="hidden" name="state" value="0">
+          <input class="form-check-input" type="checkbox" name="state" id="state" value="1" {{ old('state', 0)==1 ||
+            old('state')===null ? 'checked' : '' }}>
+          <label class="form-check-label" for="state">{{ trans('cruds.shopProduct.fields.state') }}</label>
+        </div>
+        @if($errors->has('state'))
+        <span class="text-danger">{{ $errors->first('state') }}</span>
+        @endif
+        <span class="help-block">{{ trans('cruds.shopProduct.fields.state_helper') }}</span>
+      </div>
+      <div class="form-group">
+        <button class="btn btn-danger" type="submit">
+          {{ trans('global.save') }}
+        </button>
+      </div>
+    </form>
+  </div>
 </div>
 
 
@@ -123,7 +165,7 @@
 
 @section('scripts')
 <script>
-    $(document).ready(function () {
+  $(document).ready(function () {
   function SimpleUploadAdapter(editor) {
     editor.plugins.get('FileRepository').createUploadAdapter = function(loader) {
       return {
@@ -187,7 +229,7 @@
 </script>
 
 <script>
-    var uploadedPhotosMap = {}
+  var uploadedPhotosMap = {}
 Dropzone.options.photosDropzone = {
     url: '{{ route('admin.shop-products.storeMedia') }}',
     maxFilesize: 2, // MB
@@ -246,5 +288,55 @@ Dropzone.options.photosDropzone = {
      }
 }
 
+</script>
+<script>
+  Dropzone.options.attachmentDropzone = {
+  url: '{{ route('admin.shop-products.storeMedia') }}',
+  maxFilesize: 5, // MB
+  maxFiles: 1,
+  addRemoveLinks: true,
+  headers: {
+    'X-CSRF-TOKEN': "{{ csrf_token() }}"
+  },
+  params: {
+    size: 5
+  },
+  success: function (file, response) {
+    $('form').find('input[name="attachment"]').remove()
+    $('form').append('<input type="hidden" name="attachment" value="' + response.name + '">')
+  },
+  removedfile: function (file) {
+    file.previewElement.remove()
+    if (file.status !== 'error') {
+      $('form').find('input[name="attachment"]').remove()
+      this.options.maxFiles = this.options.maxFiles + 1
+    }
+  },
+  init: function () {
+@if(isset($shopProduct) && $shopProduct->attachment)
+    var file = {!! json_encode($shopProduct->attachment) !!}
+        this.options.addedfile.call(this, file)
+    file.previewElement.classList.add('dz-complete')
+    $('form').append('<input type="hidden" name="attachment" value="' + file.file_name + '">')
+    this.options.maxFiles = this.options.maxFiles - 1
+@endif
+  },
+   error: function (file, response) {
+       if ($.type(response) === 'string') {
+           var message = response //dropzone sends it's own error messages in string
+       } else {
+           var message = response.errors.file
+       }
+       file.previewElement.classList.add('dz-error')
+       _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
+       _results = []
+       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+           node = _ref[_i]
+           _results.push(node.textContent = message)
+       }
+
+       return _results
+   }
+}
 </script>
 @endsection
